5c6e9ac7cf82f1da39963f3d6df2043de77f58fe
CristianCotovanu <cristian.cotovanu777@gmail.com>
2022-05-11 23:48:01 +0300

    Add redirect to foundation activity
    - [WIP] currently template navdraweractivity

--- app/src/main/java/com/example/refugeehelper/MainActivity.kt
+++ app/src/main/java/com/example/refugeehelper/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.refugeehelper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.refugeehelper.activities.FoundationActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.ic_foundation -> {
+                    val intent = Intent(this@MainActivity, FoundationActivity::class.java)
+                    startActivity(intent)
+                }
+                //TODO: add all other activity redirects here
+            }
+            true
+        }
     }
 }
